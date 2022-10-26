f0810f3a92d9cd765eadd8b4c77ad180913d41de
Vinicius da Silva Gomes <Viniciusgomesivv@gmail.com>
2022-10-25 22:27:51 -0300

    copyflix

---
+++ app.js
@@ -0,0 +1,14 @@
+let array = 
+
+let filme1 = "<img src='imagens/filme01.jpg'>"
+let filme2 = "<img src='imagens/filme02.jpg'>"
+let filme3 = "<img src='imagens/filme03.jpg'>"
+let filme4 = "<img src='imagens/filme04.jfif'>"
+let filme5 = "<img src='imagens/filme05.jfif'>"
+let filme6 = "<img src='imagens/filme06.jpg'>"
+document.querySelector(".container").innerHTML += filme1
+document.querySelector(".container").innerHTML += filme2
+document.querySelector(".container").innerHTML += filme3
+document.querySelector(".container").innerHTML += filme4 
+document.querySelector(".container").innerHTML += filme5 
+document.querySelector(".container").innerHTML += filme6 
